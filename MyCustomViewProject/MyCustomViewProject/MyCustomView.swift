d51860383d48b241ccc2bb0b19285ad45b1dc27b
TonyJem <a.jemanov@gmail.com>
2021-01-12 22:46:26 +0200

    Finish configuring XIB

--- MyCustomViewProject/MyCustomViewProject/MyCustomView.swift
+++ MyCustomViewProject/MyCustomViewProject/MyCustomView.swift
@@ -1,20 +1,44 @@
-//
-//  MyCustomView.swift
-//  MyCustomViewProject
-//
-//  Created by Office-iMac on 2021-01-12.
-//
-
 import UIKit
 
 class MyCustomView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    
+    @IBOutlet weak var labelForView: UILabel!
+    
+    var labelText: String {
+        get {
+            return labelForView.text!
+        }
+        set(labelText) {
+            labelForView.text = labelText
+        }
     }
-    */
-
+    
+    var workingView: UIView!
+    var xibName: String = "MyCustomView"
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setCustomView()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setCustomView()
+    }
+    
+    func getFromXib() -> UIView {
+        let bundle = Bundle(for: type(of: self))
+        let xib = UINib(nibName: xibName, bundle: bundle)
+        let view = xib.instantiate(withOwner: self, options: nil).first as! UIView
+        
+        return view
+    }
+    
+    func setCustomView() {
+        workingView = getFromXib()
+        workingView.frame = bounds
+        workingView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        
+        addSubview(workingView)
+    }
 }
