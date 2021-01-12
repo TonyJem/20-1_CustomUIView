9aa177b574a2c16f01c4553d99cb981f75871e82
TonyJem <a.jemanov@gmail.com>
2021-01-12 21:35:01 +0200

    Create file for "MyCustomView" class

---
+++ MyCustomViewProject/MyCustomViewProject/MyCustomView.swift
@@ -0,0 +1,20 @@
+//
+//  MyCustomView.swift
+//  MyCustomViewProject
+//
+//  Created by Office-iMac on 2021-01-12.
+//
+
+import UIKit
+
+class MyCustomView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
