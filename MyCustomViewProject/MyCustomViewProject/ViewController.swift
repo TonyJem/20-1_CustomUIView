bc7e0660aaf6169d1e6eca408bbf7750da4977c3
TonyJem <a.jemanov@gmail.com>
2021-01-17 11:23:19 +0200

    Revert "Remove unnessary comments from ViewController"
    This reverts commit 56c94b4f5556c5178ed237f7c9a50a6fc8162ebc.

--- MyCustomViewProject/MyCustomViewProject/ViewController.swift
+++ MyCustomViewProject/MyCustomViewProject/ViewController.swift
@@ -1,9 +1,19 @@
+//
+//  ViewController.swift
+//  MyCustomViewProject
+//
+//  Created by Office-iMac on 2021-01-12.
+//
+
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
+
+
 }
 
